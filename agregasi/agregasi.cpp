0d658c51df7e81f16fdb79d7541f8b96dd62bdcd
aivylv <lyvialarasati@gmail.com>
2024-05-22 04:26:38 +0700

    Menambahkan anak ke ibu, mencetak daftar anak, dan menghapus objek ibu

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -7,5 +7,22 @@ using namespace std;
 
 int main()
 {
-	
+	Ibu* varIbu = new Ibu("dini");
+	Ibu* varIbu2 = new Ibu("novi");
+	anak* varAnak1 = new anak("tono");
+	anak* varAnak2 = new anak("rini");
+	anak* varAnak3 = new anak("Dewi");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varIbu2->cetakAnak();
+
+	delete varIbu;
+	delete varIbu2;
+
+	return 0;
 }
