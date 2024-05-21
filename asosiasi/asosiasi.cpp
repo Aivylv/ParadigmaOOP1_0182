260fd677376a9fba134e91eea03ae4561a467000
aivylv <lyvialarasati@gmail.com>
2024-05-22 05:12:42 +0700

    mengasosiasikan pasien dgn dokter, mencetak daftar pasien dari masing2 dokter, kemudian menghapus semua objek yg dibuat

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -61,5 +61,22 @@ void Dokter::cetakPasien() {
 
 int main()
 {
-    
+	Dokter* varDokter1 = new Dokter("dr.Budi");
+	Dokter* varDokter2 = new Dokter("dr.Tono");
+	Pasien* varPasien1 = new Pasien("Andi");
+	Pasien* varPasien2 = new Pasien("Lia");
+
+	varDokter1->tambahPasien(varPasien1);
+	varDokter1->tambahPasien(varPasien2);
+	varDokter2->tambahPasien(varPasien1);
+
+	varDokter1->cetakPasien();
+	varDokter2->cetakPasien();
+
+	delete varPasien1;
+	delete varPasien2;
+	delete varDokter1;
+	delete varDokter2;
+
+	return 0;
 }
