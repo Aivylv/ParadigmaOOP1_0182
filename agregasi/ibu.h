53aca7a7429d472d745f98ad1735ae1d62cb524a
aivylv <lyvialarasati@gmail.com>
2024-05-22 04:16:30 +0700

    mendefinisikan class Ibu yg menggunakan agregasi utk mengelola objek anak dgn vector

---
+++ agregasi/ibu.h
@@ -0,0 +1,27 @@
+#ifndef IBU_H
+#define IBU_H
+
+class Ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	Ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~Ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
+void Ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak);
+}
+void Ibu::cetakAnak() {
+	cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+}
+#endif
